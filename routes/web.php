3b85674d35a9eee5a8700f091c804c8ce32f25e9
Marco Rieser <marco@rieser.dev>
2025-01-06 14:29:00 +0100

    Add testing route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Statamic\Facades\Search;
+use Statamic\Facades\Site;
 
-// Route::statamic('example', 'example-view', [
-//    'title' => 'Example'
-// ]);
+Route::get('/{site}', function ($site) {
+     Site::setCurrent($site);
+
+    return Search::index('posts')
+        ->ensureExists()
+        ->search('')
+        ->where('status', 'published')
+        ->where('site', Site::current())
+        ->orderBy('order_number')
+        ->orderBy('title')
+        ->get()
+        ->select('title', 'order_number', 'locale');
+ });
